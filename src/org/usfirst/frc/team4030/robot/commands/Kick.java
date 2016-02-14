c2317326daec6a10724e4214c097add68c5ec6d6
Zachary Kotlarek <zach@kotlarek.com>
2016-02-14 14:26:52 -0800

    Kick and Capture

---
+++ src/org/usfirst/frc/team4030/robot/commands/Kick.java
@@ -0,0 +1,37 @@
+package org.usfirst.frc.team4030.robot.commands;
+
+import org.usfirst.frc.team4030.robot.Robot;
+import edu.wpi.first.wpilibj.command.Command;
+
+public class Kick extends Command {
+	
+	// Kick motion duration in ms
+	public static final int duration = 500;
+	
+	// When should we stop
+	private long done = 0;
+
+	public Kick() {
+    	requires(Robot.kicker);
+	}
+
+	protected void initialize() {
+		done = System.currentTimeMillis() + duration;
+	}
+	
+	protected void execute() {
+		Robot.kicker.kick();
+	}
+
+	protected boolean isFinished() {
+		return System.currentTimeMillis() >= done;
+	}
+
+	protected void end() {
+		Robot.kicker.stop();
+	}
+
+	protected void interrupted() {
+		this.end();
+	}
+}
--- src/org/usfirst/frc/team4030/robot/subsystems/Kicker.java
+++ src/org/usfirst/frc/team4030/robot/subsystems/Kicker.java
@@ -5,18 +5,27 @@ import org.usfirst.frc.team4030.robot.RobotMap;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-public class Kicker extends Subsystem {	
+public class Kicker extends Subsystem {
 	Talon motor = new Talon(RobotMap.pwmKicker);
-
-	public Kicker() {
-	}
+	public static final double speed = 0.5;
 	
-	public void stop() {
-		motor.disable();
+	public Kicker() {
 	}
 
 	@Override
 	protected void initDefaultCommand() {
-		// TODO Auto-generated method stub
+		// No default command
+	}
+
+	public void kick() {
+		motor.set(speed);
+	}
+
+	public void capture() {
+		motor.set(-speed);
+	}
+
+	public void stop() {
+		motor.disable();
 	}
 }
