49adc8888e989caa89d7cd216664fb5d02b65639
Zachary Kotlarek <zach@kotlarek.com>
2016-02-14 16:16:19 -0800

    Match actual hardware

--- src/org/ingrahamrobotics/robot/RobotMap.java
+++ src/org/ingrahamrobotics/robot/RobotMap.java
@@ -15,16 +15,18 @@ public class RobotMap {
 	public static final int pwmDriveLeft = 0;
 	public static final int pwmDriveRight = 1;
 	public static final int pwmArm = 2;
-	public static final int pwmKicker = 3;
-	public static final int pwmShooter = 4;
+	public static final int pwmShooter = 3;
+	public static final int pwmKicker = 4;
 	
 	// DIO
-	public static final int dioDriveLeftA = 0;
-	public static final int dioDriveLeftB = 1;
-	public static final int dioDriveRightA = 2;
-	public static final int dioDriveRightB = 3;
-	public static final int dioArmA = 4;
-	public static final int dioArmB = 5;
-	public static final int dioArmSwitch = 6;
-	public static final int dioShooter = 7;
+	public static final int dioArmSwitch = 0;
+	public static final int dioArmA = 1;
+	public static final int dioArmB = 2;
+	public static final int dioShooter = 3;
+
+	// Speculative DIO
+	public static final int dioDriveLeftA = 4;
+	public static final int dioDriveLeftB = 5;
+	public static final int dioDriveRightA = 6;
+	public static final int dioDriveRightB = 7;
 }
