b8289ad9182be91bd1bccf6f993c3f982209cdab
Zachary Kotlarek <zach@kotlarek.com>
2016-03-05 00:04:40 -0800

    Add a general-purpose PIDWait command, since the provided .onTarget() does not seem to work

--- src/org/ingrahamrobotics/robot/commands/ShooterWait.java
+++ src/org/ingrahamrobotics/robot/commands/ShooterWait.java
@@ -1,59 +1,14 @@
 package org.ingrahamrobotics.robot.commands;
 
 import org.ingrahamrobotics.robot.Robot;
-import org.ingrahamrobotics.robot.output.Output;
-import org.ingrahamrobotics.robot.output.OutputLevel;
-import org.ingrahamrobotics.robot.subsystems.Sensors;
 
-import edu.wpi.first.wpilibj.command.Command;
+public class ShooterWait extends WaitPID {
 
-public class ShooterWait extends Command {
-
-	public static final int kREADY_MIN = 3;
-	private int ready = 0;
+	// Not configurable because these are programming features not runtime features
+	public static final double kTOLERANCE = 0;
+	public static final int kMIN_SUCCESS = 3;
 
 	public ShooterWait() {
-	}
-
-	protected void initialize() {
-		ready = 0;
-	}
-
-	protected void execute() {
-    	Output.output(OutputLevel.PID, getName() + "-ready", ready);
-	}
-
-	// Done when we're at or above the PID setpoint for kREADY_MIN samples
-	protected boolean isFinished() {
-		double actual = Sensors.Sensor.SHOOTER_ENCODER.getDouble();
-		double setpoint = Robot.shooter.getSetpoint();
-
-		// Waiting for 0 makes no sense
-		// Presumably someone is about to set a valid setpoint so just wait
-		if (setpoint < 1) {
-			return false;
-		}
-
-		// Mark as ready if we're at or above the setpoint
-		if (actual >= setpoint) {
-			ready++;
-		}
-
-		// Done after the minimum number of ready samples
-		if (ready > kREADY_MIN) {
-			return true;
-		}
-
-		// Otherwise we're still waiting
-		return false;
-	}
-
-	protected void end() {
-    	ready = 0;
-    	Output.output(OutputLevel.PID, getName() + "-ready", ready);
-	}
-
-	protected void interrupted() {
-		this.end();
+		super(Robot.shooter, kTOLERANCE, Mode.kABS_MIN, kMIN_SUCCESS);
 	}
 }
