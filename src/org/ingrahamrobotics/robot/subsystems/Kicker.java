437d8be91e277aa8baff440ce1f82a75213d3c52
Zachary Kotlarek <zach@kotlarek.com>
2016-03-09 00:21:45 -0800

    Log shots when logging vision

--- src/org/ingrahamrobotics/robot/subsystems/Kicker.java
+++ src/org/ingrahamrobotics/robot/subsystems/Kicker.java
@@ -1,5 +1,11 @@
 package org.ingrahamrobotics.robot.subsystems;
 
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.ingrahamrobotics.robot.Robot;
 import org.ingrahamrobotics.robot.RobotMap;
 import org.ingrahamrobotics.robot.output.Output;
 import org.ingrahamrobotics.robot.output.OutputLevel;
@@ -20,6 +26,22 @@ public class Kicker extends Subsystem {
 	public void kick() {
 		double speed = Settings.Key.KICKER_SPEED.getDouble();
 		set(speed);
+		save();
+	}
+
+	private void save() {
+		if (!CameraTarget.kENABLE_VISION_LOG) {
+			return;
+		}
+
+		Path file = Paths.get(Robot.camTarget.getVisionDir().toString(), System.currentTimeMillis() + "-kick.txt");
+		try {
+			PrintWriter out = new PrintWriter(file.toFile());
+			out.println("A:" + Sensors.Sensor.ARM_ENCODER.getInt() + ";S:" + Sensors.Sensor.SHOOTER_ENCODER.getDouble());
+			out.close();
+		} catch (FileNotFoundException e) {
+			System.err.println("Unable to save kick file");
+		}
 	}
 
 	public void capture() {
