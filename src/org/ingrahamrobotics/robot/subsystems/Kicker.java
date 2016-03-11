df9ad7dc008a126906728be34c0c7e9ba837c421
Zachary Kotlarek <zach@kotlarek.com>
2016-03-11 10:57:21 -0800

    Disable logging when the disk is nearly full

--- src/org/ingrahamrobotics/robot/subsystems/Kicker.java
+++ src/org/ingrahamrobotics/robot/subsystems/Kicker.java
@@ -1,10 +1,5 @@
 package org.ingrahamrobotics.robot.subsystems;
 
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-
 import org.ingrahamrobotics.robot.RobotMap;
 import org.ingrahamrobotics.robot.output.Output;
 import org.ingrahamrobotics.robot.output.OutputLevel;
@@ -16,7 +11,12 @@ import edu.wpi.first.wpilibj.command.Subsystem;
 
 public class Kicker extends Subsystem {
 
-	Talon motor = new Talon(RobotMap.pwmKicker);
+	private Log log;
+	private Talon motor = new Talon(RobotMap.pwmKicker);
+
+	public Kicker() {
+		log = new Log();
+	}
 
 	@Override
 	protected void initDefaultCommand() {
@@ -26,22 +26,9 @@ public class Kicker extends Subsystem {
 	public void kick() {
 		double speed = Settings.Key.KICKER_SPEED.getDouble();
 		set(speed);
-		save();
-	}
 
-	private void save() {
-		if (!Log.kENABLE_VISION_LOG) {
-			return;
-		}
-
-		Path file = Paths.get(Log.getPath().toString(), System.currentTimeMillis() + "-kick.txt");
-		try {
-			PrintWriter out = new PrintWriter(file.toFile());
-			out.println("A:" + Sensors.Sensor.ARM_ENCODER.getInt() + ";S:" + Sensors.Sensor.SHOOTER_ENCODER.getDouble());
-			out.close();
-		} catch (FileNotFoundException e) {
-			System.err.println("Unable to save kick file");
-		}
+		String str = "A:" + Sensors.Sensor.ARM_ENCODER.getInt() + ";S:" + Sensors.Sensor.SHOOTER_ENCODER.getDouble();
+		log.save(str, System.currentTimeMillis() + "-kick.txt");
 	}
 
 	public void capture() {
