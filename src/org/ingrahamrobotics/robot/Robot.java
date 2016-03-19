80c954e199f0c692cc56ba1d2041905577cf3463
4030 Computer <robotpc@robotics>
2016-03-19 10:51:46 -0700

    Enable easier testing

--- src/org/ingrahamrobotics/robot/Robot.java
+++ src/org/ingrahamrobotics/robot/Robot.java
@@ -43,7 +43,7 @@ public class Robot extends IterativeRobot {
 	public static Class<? extends Command> driveCmd = null;
 	public static final boolean disableShooterPID = true; // True until the encoder works
 	public static final boolean disableReadPower = !production;
-	public static final boolean disableProdControls = !production;
+	public static final boolean disableProdControls = false;//!production;
 	public static final boolean disableTestControls = production;
 	public static final boolean disableCamTarget = false;
 
