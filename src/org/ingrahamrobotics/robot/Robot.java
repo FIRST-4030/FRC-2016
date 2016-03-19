ac9182df2503d2d76590527b4045c246c1ce947f
4030 Computer <robotpc@robotics>
2016-03-19 12:51:33 -0700

    Disable camera after bad NIVision crashes

--- src/org/ingrahamrobotics/robot/Robot.java
+++ src/org/ingrahamrobotics/robot/Robot.java
@@ -45,7 +45,7 @@ public class Robot extends IterativeRobot {
 	public static final boolean disableReadPower = true;
 	public static final boolean disableProdControls = !production;
 	public static final boolean disableTestControls = production;
-	public static final boolean disableCamTarget = false;
+	public static final boolean disableCamTarget = true;
 
 	@SuppressWarnings("unused")
 	public void robotInit() {
